6dc71a6b3335d15e03c402d961676c21715f43ba
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-07-13 22:30:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import mailparser
 
 
 # Setting page title and header
-st.set_page_config(page_title="Tagu", page_icon=":robot:")
+st.set_page_config(page_title="Tagu", page_icon="::robot::")
 st.markdown("<h1 style='text-align: center;'> 🤖 Tagu: RCA GENERATOR 🤖 </h1>", unsafe_allow_html=True)
 st.markdown("""---""")
 
