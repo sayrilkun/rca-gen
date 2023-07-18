0b9bee413927a6183a156d057f8a7a10a456b68e
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-07-18 16:13:29 +0800

    afd

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import mailparser
 # from PIL import Image
 
 # Setting page title and header
-st.set_page_config(page_title="Ruth", page_icon= "https://api.dicebear.com/6.x/bottts/svg?seed=Garfield")
+icon = st.image("https://api.dicebear.com/6.x/bottts/svg?seed=Garfield")
+st.set_page_config(page_title="Ruth", page_icon= icon)
 st.markdown("<h1 style='text-align: center;'> 🤖 ![Alt Text](https://api.dicebear.com/6.x/bottts/svg?seed=Garfield) Ruth: RCA GENERATOR 🤖 </h1>", unsafe_allow_html=True)
 st.markdown("""---""")
 # fis = Image.open('fis.jpg')
