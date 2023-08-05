d092e39405aa29b13a44e7e98620a3fa296e9c4d
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-05 23:43:58 +0800

    nm

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
     
 # Map model names to OpenAI model IDs
-model_name = "gpt-3.5-turbo"
+model = "gpt-3.5-turbo"
 
 # if model_name == "GPT-3.5":
 #     model = "gpt-3.5-turbo"
@@ -59,11 +59,11 @@ if clear_button:
         {"role": "system", "content": "You are a helpful assistant."}
     ]
     st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
+    # st.session_state['model_name'] = []
     st.session_state['cost'] = []
     st.session_state['total_cost'] = 0.0
     st.session_state['total_tokens'] = []
-    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    # counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
 
 # generate a response
@@ -89,11 +89,11 @@ def prompt(user_input):
         output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
-        st.session_state['model_name'].append(model_name)
+        # st.session_state['model_name'].append(model_name)
         st.session_state['total_tokens'].append(total_tokens)
 
         # from https://openai.com/pricing#language-models
-        if model_name == "GPT-3.5":
+        if model_name == "gpt-3.5-turbo":
             cost = total_tokens * 0.002 / 1000
         else:
             cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
