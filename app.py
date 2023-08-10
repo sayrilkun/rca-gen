a9a41a3bdd091695f79506bd49b8f3198117b091
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-10 15:59:22 +0800

    adsfsdf

--- app.py
+++ app.py
@@ -178,9 +178,11 @@ if st.session_state['generated']:
             message(st.session_state["generated"][i], key=str(i), avatar_style="bottts", seed = "Sophie")
             st.write(st.session_state["generated"][0])
             # df = pd.DataFrame(st.session_state["generated"][0])
-            df = pd.DataFrame(eval(st.session_state["generated"][0]))
-            st.table(df)
-
+            try:
+                df = pd.DataFrame(eval(st.session_state["generated"][0]))
+                st.table(df)
+            except Exception as e:
+                continue
             # st.write(
             #     f"Model used: GPT 3.5; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
             # counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
