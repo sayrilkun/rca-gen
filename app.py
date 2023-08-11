d1afdbb47177c7a808b58c4d57775071e2727f2c
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-11 09:34:04 +0800

    efadsfa

--- app.py
+++ app.py
@@ -177,6 +177,18 @@ if st.session_state['generated']:
             # message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="croodles", seed="Tigger")
             message(st.session_state["generated"][i], key=str(i), avatar_style="bottts", seed = "Sophie")
 
+
+            st.header("☢️ RCA Details")
+            st.subheader("☢️ Root Cause")
+            st.subheader("☢️ Investigation & Resolution")
+
+            st.subheader("☢️ Contributing Factors")
+
+            st.header("☢️ Action Items")
+
+            st.header("☢️ RCA 5 WHYs")
+            
+            st.header("☢️ Incident Timeline")
             try:
                 df = pd.DataFrame(eval(st.session_state["generated"][0]))
                 st.table(df)
