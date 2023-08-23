a88977946d5a06cd3f49710fee1ecaa6183d82d8
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-23 15:41:05 +0800

    dsfsafsd

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def prompt(user_input):
 # Setting page title and header
 
 st.set_page_config(page_title="Ruth", page_icon= ":flower:")
-st.markdown("<h1 style='text-align: center;'> 🤖 Ruth: RCA GENERATOR TESTasd🤖 </h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'> 🤖 Ruth: RCA GENERATOR 🤖 </h1>", unsafe_allow_html=True)
 st.markdown("""---""")
 
 
