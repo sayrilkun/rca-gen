d2fcc72d7acbbe1e9d11769c17aea7fb5e8847bd
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-29 11:25:03 +0800

    afdad

--- app.py
+++ app.py
@@ -160,6 +160,8 @@ if st.session_state['generated']:
             if file is True:
                 prompt(prompts.rca_details_prompt)
                 st.write(st.session_state["generated"][1])
+                rca_details_df = pd.DataFrame(eval(st.session_state["generated"][1]))
+                st.table(rca_details_df)
 
 
         st.subheader("☢️ Root Cause")
