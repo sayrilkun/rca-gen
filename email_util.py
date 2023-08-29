c28d5577acadff0caba268a23dfc55279f9cd59d
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-08-29 10:14:26 +0800

    fasdfa

--- email_util.py
+++ email_util.py
@@ -1,5 +1,6 @@
 import mailparser
-
+import streamlit as st
 def parse_from_bytes (bytes_data):
     mail = mailparser.parse_from_bytes(bytes_data)
+    st.write(mail.text_plain)
     return mail.text_plain
