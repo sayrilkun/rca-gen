f76d9258e34e31b64e91eb5a3ba9abae9f4f6f4f
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-08 09:52:54 +0800

    rca details

--- prompts.py
+++ prompts.py
@@ -13,15 +13,37 @@ inc_timeline_format =     [
 }
 ]
 
+#WORKING PROMPT
+# rca_details_format = [
+#     {
+#         "Root Cause" : "Root Cause",
+#         "RCA Executive Summary" : "RCA Executive Summary"
+#     }]
+
+
+# rca_details_prompt = f''' You are an Incident Analyst. 
+# Identify the Root cause of the incident and make a lengthy executive summary, minimum 2 paragraphs based on the 
+# given email thread. Include important events happened in the thread in your executive summary.
+
+# I want your output to be a Python Dataframe like this format below.
+
+# {rca_details_format}
+
+# '''
+
 rca_details_format = [
     {
-        "Root Cause" : "Root Cause",
-        "RCA Executive Summary" : "RCA Executive Summary"
+        "Root Cause" : "Answer #1",
+        "RCA Executive Summary" : "Answer #2",
+        "Investigation and Resolution" : "Answer #3"
+        "Contributing Factors" : "Answer #4"
     }]
+rca_details_prompt = f''' You are an Incident Analyst. I want you to answer 4 things for me based on your profession.
 
-rca_details_prompt = f''' You are an Incident Analyst. 
-Identify the Root cause of the incident and make a lengthy executive summary, minimum 2 paragraphs based on the 
-given email thread. Include important events happened in the thread in your executive summary.
+1. Identify the Root cause of the incident 
+2. Generate a lengthy executive summary, minimum 2 paragraphs, based on the given email thread. Include important events happened in the thread in your executive summary.
+3. What are the key dates that leads to investigation and resolution. Explain each dates.
+4. What are the factors that contributed to the existing issue.
 
 I want your output to be a Python Dataframe like this format below.
 
