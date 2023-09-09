77684dad6abc821eba25a5ec2e327a1069cd6077
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-09 17:07:26 +0800

    fix

--- app.py
+++ app.py
@@ -197,42 +197,42 @@ if st.session_state['generated']:
             docx_util.build_word_document(eval(st.session_state["generated"][0]))
 
             # SECOND PROMPT (RCA DETAILS)
-            st.header("☢️ RCA Details")
-            rca_details_button = st.button("Generate RCA Details :rocket:", key="rca_details",use_container_width=True)
-            if rca_details_button:
-                # if file is True:
-                prompt(prompts.rca_details_prompt)
-                st.write(st.session_state["generated"][1])
-                try:
-                    rca_details_df = pd.DataFrame(eval(st.session_state["generated"][1]))
-                    # st.table(rca_details_df)
+        st.header("☢️ RCA Details")
+        rca_details_button = st.button("Generate RCA Details :rocket:", key="rca_details",use_container_width=True)
+        if rca_details_button:
+            # if file is True:
+            prompt(prompts.rca_details_prompt)
+            st.write(st.session_state["generated"][1])
+            try:
+                rca_details_df = pd.DataFrame(eval(st.session_state["generated"][1]))
+                # st.table(rca_details_df)
 
-                    st.subheader("☢️ Root Cause")
-                    st.success(rca_details_df.iloc[0, 0])
+                st.subheader("☢️ Root Cause")
+                st.success(rca_details_df.iloc[0, 0])
 
-                    st.subheader("☢️ RCA Executive Summary")
-                    st.success(rca_details_df.iloc[0, 1])
+                st.subheader("☢️ RCA Executive Summary")
+                st.success(rca_details_df.iloc[0, 1])
 
-                    st.subheader("☢️ Investigation & Resolution")
-                    st.success(rca_details_df.iloc[0, 2])
+                st.subheader("☢️ Investigation & Resolution")
+                st.success(rca_details_df.iloc[0, 2])
 
-                    st.subheader("☢️ Contributing Factors")
-                
-                except Exception as e:
+                st.subheader("☢️ Contributing Factors")
+            
+            except Exception as e:
+                pass
+
+            # file = False
+
+        st.header("☢️ Action Items")
+        action_items_button = st.button("Generate Action Items :rocket:", key="action_items",use_container_width=True)
+        if action_items_button:
+            prompt(prompts.action_items_prompt)
+            st.write(st.session_state["generated"][2])
+            try:
+                action_items_df = pd.DataFrame(eval(st.session_state["generated"][2]))
+                st.table(action_items_df)
+            except Exception as e:
                     pass
-
-                # file = False
-
-            st.header("☢️ Action Items")
-            action_items_button = st.button("Generate Action Items :rocket:", key="action_items",use_container_width=True)
-            if action_items_button:
-                prompt(prompts.action_items_prompt)
-                st.write(st.session_state["generated"][2])
-                try:
-                    action_items_df = pd.DataFrame(eval(st.session_state["generated"][2]))
-                    st.table(action_items_df)
-                except Exception as e:
-                        pass
 
         st.header("☢️ RCA 5 WHYs")
         five_whys_button = st.button("Generate 5 WHYs :rocket:", key="five_whys",use_container_width=True)
