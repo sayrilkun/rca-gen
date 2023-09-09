d4736965397e1083016ba68f887d8a84315787d7
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-09 10:21:48 +0800

    GH

--- app.py
+++ app.py
@@ -200,26 +200,26 @@ if st.session_state['generated']:
         st.header("☢️ RCA Details")
         rca_details_button = st.button("Generate RCA Details :rocket:", key="rca_details",use_container_width=True)
         if rca_details_button:
-            if file is True:
-                prompt(prompts.rca_details_prompt)
-                st.write(st.session_state["generated"][1])
-                try:
-                    rca_details_df = pd.DataFrame(eval(st.session_state["generated"][1]))
-                    # st.table(rca_details_df)
+            # if file is True:
+            prompt(prompts.rca_details_prompt)
+            st.write(st.session_state["generated"][1])
+            try:
+                rca_details_df = pd.DataFrame(eval(st.session_state["generated"][1]))
+                # st.table(rca_details_df)
 
-                    st.subheader("☢️ Root Cause")
-                    st.success(rca_details_df.iloc[0, 0])
+                st.subheader("☢️ Root Cause")
+                st.success(rca_details_df.iloc[0, 0])
 
-                    st.subheader("☢️ RCA Executive Summary")
-                    st.success(rca_details_df.iloc[0, 1])
+                st.subheader("☢️ RCA Executive Summary")
+                st.success(rca_details_df.iloc[0, 1])
 
-                    st.subheader("☢️ Investigation & Resolution")
-                    st.success(rca_details_df.iloc[0, 2])
+                st.subheader("☢️ Investigation & Resolution")
+                st.success(rca_details_df.iloc[0, 2])
 
-                    st.subheader("☢️ Contributing Factors")
-                
-                except Exception as e:
-                    pass
+                st.subheader("☢️ Contributing Factors")
+            
+            except Exception as e:
+                pass
 
                 # file = False
 
