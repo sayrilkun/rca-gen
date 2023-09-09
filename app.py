602ab635df77d9175d136b28995dc6107e05b65c
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-09 17:19:37 +0800

    lk

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ if st.session_state['generated']:
     with response_container:
         for i in range(len(st.session_state['generated'])):
             # HIDING THE CHATBOX
-            # message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="croodles", seed="Tigger")
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="croodles", seed="Tigger")
             message(f'XX{i}XX {st.session_state["generated"][i]} ', key=str(i), avatar_style="bottts", seed = "Sophie")
             log.info(st.session_state['generated'])
             try:
