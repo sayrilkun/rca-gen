62bd210378a40ee68918cdc0684ed1ce7f039622
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-12 22:06:21 +0800

    adsad

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -37,11 +37,11 @@ with search_container:
     for i in range(len(search_results)):
         st.write(search_results[i])
         st.write(f"Type  {type(search_results[i])}")
-        # existing_item = container.read_item(
-        # item= hi,
-        # partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",)
-        # st.write(f"Item: {item}")
-        # st.write(f"{existing_item}")
+        existing_item = container.read_item(
+        item= search_results[i],
+        partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",)
+        st.write(f"Item: {item}")
+        st.write(f"{existing_item}")
 
 
 if search_state is False:
