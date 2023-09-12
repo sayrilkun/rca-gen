7a1e94b7d0f4228f3b30595c2823ce306b6f3443
Kitts <Johndell.Kitts@FISGLOBAL.COM>
2023-09-12 10:17:30 +0800

    Removed external library

--- lib/email_parser.py
+++ lib/email_parser.py
@@ -2,7 +2,6 @@
 # Imports
 #
 import ruthinit
-from external import outlookmsgfile
 from io import BytesIO
 from extract_msg import Message
 
@@ -14,18 +13,6 @@ log = ruthinit.log
 #
 # Functions
 #
-def convert_msg_to_eml(file):
-    '''
-    call external library to convert msg to eml
-
-    parameters:
-        file - .msg file to convert
-
-    return:
-        eml - email object of .eml file
-    '''
-    eml = outlookmsgfile.load(file)
-    return eml
 
 def convert_msg_to_text(file):
     '''
