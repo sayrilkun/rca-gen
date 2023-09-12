4cac94cf08889c71433d8a846ab0f48ba6ab3a13
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-12 22:52:28 +0800

    cszdf

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -66,7 +66,8 @@ with search_container:
 
 if search_state is False:
     for item in items:
-        with st.expander(f'''\n Incident Name: {item["incidentName"]} \n Date Uploaded: {item["incidentDate"]} \n
+        with st.expander(f''' Incident Name: {item["incidentName"]} \n 
+        Date Uploaded: {item["incidentDate"]} \n
         '''):
             search_rca_details_df = pd.DataFrame(eval(item["rcaDetails"]))
             st.write("Root Cause")
