df62875fe006e2b5aa1bd5babe45710e95370568
sayrilkun <34339698+sayrilkun@users.noreply.github.com>
2023-09-13 09:48:04 +0800

    adsfa

--- Home.py
+++ Home.py
@@ -196,7 +196,7 @@ if generate_button:
 # container for chat history
 response_container = st.container()
 # container for text box
-container = st.container()
+rca_container = st.container()
 
 # chat box
 # with container:
